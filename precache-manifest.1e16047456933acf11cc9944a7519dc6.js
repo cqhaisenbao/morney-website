da90b5a54ac2b3d4c955f557b41beca9ccf4387d
cqhaisenbao <fanyu9353@gmail.com>
2020-12-17 15:40:21 +0800

    deploy

---
+++ precache-manifest.1e16047456933acf11cc9944a7519dc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142f530df202b689af12",
+    "url": "/morney-website/css/app.7a1c490c.css"
+  },
+  {
+    "revision": "3e5ee035e9c8c3250d88",
+    "url": "/morney-website/css/chunk-vendors.0d6d9e38.css"
+  },
+  {
+    "revision": "d7202ff9d35d07896617457c0615bbbd",
+    "url": "/morney-website/favicon.png"
+  },
+  {
+    "revision": "52d69442b9142c5065c86605ec0eb5aa",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "142f530df202b689af12",
+    "url": "/morney-website/js/app.130a14d1.js"
+  },
+  {
+    "revision": "3e5ee035e9c8c3250d88",
+    "url": "/morney-website/js/chunk-vendors.103d6e06.js"
+  },
+  {
+    "revision": "e07292a4c1f8cca8ad20b841dcbcbaa9",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
